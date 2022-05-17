deaf20798ef81f3a59819ebb3def35f7a054b126
Tr4shL0rd <ketil04@gmail.com>
2022-05-17 14:04:04 +0200

    evo chain

--- evoChain.py
+++ evoChain.py
@@ -1,7 +1,20 @@
+from itertools import chain
 import json
 import requests
 from rich import print as rprint
-pokeName = "charmander"
+
+def chaining(evos:list):
+    for i in range(len(evos)):
+        for elem in evos:
+            if elem == "None":
+                evos.remove(elem)
+    if len(evos) == 3:
+        return f"{evos[0]} -> {evos[1]} -> {evos[2]}"
+    elif len(evos) == 2:
+        return f"{evos[0]} -> {evos[1]}"
+    elif len(evos) == 1:
+        return f"{evos[0]}"
+pokeName = "pinsir"
 url = f"https://pokeapi.co/api/v2/pokemon/{pokeName}/"
 r = requests.get(url)
 data = r.json()
@@ -15,7 +28,17 @@ r = requests.get(evoChain)
 evoChainData = r.json()
 
 evoOneName   = pokeName
-evoTwoName   = evoChainData["chain"]["evolves_to"][0]["species"]["name"]
-evoThreeName = evoChainData["chain"]["evolves_to"][0]["evolves_to"][0]["species"]["name"]
-
-print(f"{evoOneName} -> {evoTwoName} -> {evoThreeName}")
+try:
+    evoTwoName   = evoChainData["chain"]["evolves_to"][0]["species"]["name"]
+except IndexError:
+    evoTwoName   = "None"
+try:
+    evoThreeName = evoChainData["chain"]["evolves_to"][0]["evolves_to"][0]["species"]["name"]
+except IndexError:
+    evoThreeName   = "None"
+evos = []
+evos.append(evoOneName)
+evos.append(evoTwoName)
+evos.append(evoThreeName)
+print(chaining(evos))
+#print(f"{evos[0]} -> {evos[1]} -> {evos[2]}")
